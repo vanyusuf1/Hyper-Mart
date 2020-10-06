c5576b1e0208dca5f3d860afd50dd045bbaa9c1f
vanyusuf1 <yusufvan71@gmail.com>
2020-10-06 12:01:39 +0530

    added middlewares

---
+++ routes/auth.js
@@ -0,0 +1,7 @@
+var express = require("express");
+var router = express.Router();
+const { signout } = require("../controllers/auth");
+
+router.get("/signout", signout);
+
+module.exports = router;
