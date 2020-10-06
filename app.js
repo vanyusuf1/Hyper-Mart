e027bcccdf3feb1357de3b1c3206dd2d2c1c82bc
vanyusuf1 <yusufvan71@gmail.com>
2020-10-06 11:40:57 +0530

    Update Changes

--- app.js
+++ app.js
@@ -8,13 +8,19 @@ mongoose
   .connect(process.env.DATABASE, {
     useNewUrlParser: true,
     useUnifiedTopology: true,
-    useCreateIndex: true
+    useCreateIndex: true,
   })
   .then(() => {
     console.log("DB CONNECTED");
-  });
+  })
+  .catch(console.log("Db got oops"));
 
 const port = process.env.PORT || 8000;
+const admin=(req,res=>{
+  return res.send("this is admin dashboard.")
+});
+    
+
 
 app.listen(port, () => {
   console.log(`app is running at ${port}`);
