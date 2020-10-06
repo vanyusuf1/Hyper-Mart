9dbe44741d767b6af9fbac9f059b39af1f4a974c
vanyusuf1 <yusufvan71@gmail.com>
2020-10-06 12:04:12 +0530

    Added Middlewares

--- app.js
+++ app.js
@@ -3,25 +3,35 @@ require("dotenv").config();
 const mongoose = require("mongoose");
 const express = require("express");
 const app = express();
+const bodyParser = require("body-parser");
+const cookieParser = require("cookie-parser");
+const cors = require("cors");
 
+const authRoutes = require("./routes/auth");
+
+//DB Connection
 mongoose
   .connect(process.env.DATABASE, {
     useNewUrlParser: true,
     useUnifiedTopology: true,
-    useCreateIndex: true,
+    useCreateIndex: true
   })
   .then(() => {
     console.log("DB CONNECTED");
-  })
-  .catch(console.log("Db got oops"));
+  });
 
+//Middlewares
+app.use(bodyParser.json());
+app.use(cookieParser());
+app.use(cors());
+
+//My Routes
+app.use("/api", authRoutes);
+
+//PORT
 const port = process.env.PORT || 8000;
-const admin=(req,res=>{
-  return res.send("this is admin dashboard.")
-});
-    
 
-
+//Starting a server
 app.listen(port, () => {
   console.log(`app is running at ${port}`);
 });
