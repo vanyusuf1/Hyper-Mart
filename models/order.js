4a82279bc48a5f08583dfb8a38180772aeb26c46
vanyusuf1 <yusufvan71@gmail.com>
2020-10-06 11:40:34 +0530

    Update Changes

---
+++ models/order.js
@@ -0,0 +1,29 @@
+const mongosse = require("mongoose");
+const { ObjectId } = mongoose.Schema;
+const ProductCartSchema = new mongosse.Schema({
+  product: {
+    type: ObjectId,
+    ref: "Product",
+  },
+  name: String,
+  count: Number,
+  price: Number,
+});
+const ProductCart = mongoose.model("ProductCart", ProductCartSchema);
+const OrderSchema = new mongosse.Schema(
+  {
+    products: [ProductCartSchema],
+    transaction_id: {},
+    amount: { type: Number },
+    address: String,
+    updated: Date,
+    user: {
+      type: ObjectId,
+      ref: "User",
+    },
+  },
+  { timestamps: true }
+);
+
+const Order = mongoose.model("Order", OrderSchema);
+module.exports = { Order, ProductCart };
